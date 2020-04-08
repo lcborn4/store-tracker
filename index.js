e9b8e6bf465ff36cdb1fcfadee717885a8d4aefb
Logan <lcborn4@gmail.com>
2020-04-07 22:43:52 -0400

    headless true

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ setInterval(checkSlot, STORE_CHECK_TIMER);
 
 async function checkSlot() {
     console.log('checkSlot')
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
     await page.goto(url);
 
